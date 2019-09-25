98a059b16ff567a1b41a7b9090879049d5b9dc94
Mario Perez <mario.perez415@gmail.com>
2019-09-24 23:36:03 -0400

    Create controller component and pass project state down to presentation comps

--- source/components/portfolio/PortfolioPage.js
+++ source/components/portfolio/PortfolioPage.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import getProjects from '../../api/cmsApi';
+import ProjectList from './ProjectList';
 
 const PortfolioPage = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        if (projects.length === 0) {
+            getProjects().then(response => {
+                console.log(response);
+                setProjects(response.items);
+            });
+        }
+    }, []);
     return (
-        <section className='-wrap -exp-3'>
-            <h1>Portfolio</h1>
-        </section>
+        <main className='page'>
+            <section className='page__section -tall -bg--dark'></section>
+            <section className='page__section -wrap -exp-3'>
+                <div className='page__section__inner'>
+                    <h2>Portfolio</h2>
+                    <hr />
+                    <ProjectList projects={projects} />
+                </div>
+            </section>
+        </main>
     );
 };
 
