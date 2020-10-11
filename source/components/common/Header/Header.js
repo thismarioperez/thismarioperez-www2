9fd0ab727cb89e6858ebcc0fe6686eed12526ef1
Mario Perez <mario.perez415@gmail.com>
2020-10-11 17:22:42 -0400

    More file structure cleanup

--- source/components/common/Header/Header.js
+++ source/components/common/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "./Logo";
-import LogoMark from "./LogoMark";
+import Logo from "../Logo";
+import LogoMark from "../LogoMark";
 import "./Header.scss";
 
 const Header = () => {
