824388ffbffde6588e71986013764008a27e961d
Mario <mario.perez415@gmail.com>
2020-10-19 20:29:39 -0400

    Get intro animation setup. TODO: Fix stuttering between svg group in/out transition

--- source/components/Pages/IntroPage/IntroPage.js
+++ source/components/Pages/IntroPage/IntroPage.js
@@ -1,22 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Screen from "../../common/Screen";
 import IntroSvg from "./IntroSvg";
 import "./Intro.scss";
 
 const IntroPage = () => {
-    const [showScreen, setShowScreen] = useState(true);
+    const [showScreen, setShowScreen] = useState(false);
     const screenProps = {
         className: "intro",
         showScreen,
         timeout: showScreen ? 0 : 800,
     };
+    useEffect(() => {
+        setShowScreen(true)
+    }, []);
     return (
         <Screen {...screenProps}>
             <div className="intro">
                 <div className="intro__inner">
                     <div className="-wrap">
                         <div className="intro__welcome">
-                            <IntroSvg />
+                            <IntroSvg setShowScreen={setShowScreen}/>
                         </div>
                     </div>
                 </div>
